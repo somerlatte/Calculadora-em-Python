a1aafebd9cbd31001a0e1c5968a03b9d7a6be693
Lorrayne Somerlatte <75647071+somerlatte@users.noreply.github.com>
2023-06-02 22:01:05 -0300

    Create test_calculadora.py

---
+++ test_calculadora.py
@@ -0,0 +1,28 @@
+import pytest
+from calculadora import Calculadora
+
+
+def test_adicao():
+    calculadora = Calculadora()
+    resultado = calculadora.adicao(2, 3)
+    assert resultado == 5
+
+def test_subtracao():
+    calculadora = Calculadora()
+    resultado = calculadora.subtracao(5, 3)
+    assert resultado == 2
+
+def test_multiplicacao():
+    calculadora = Calculadora()
+    resultado = calculadora.multiplicacao(2, 3)
+    assert resultado == 6
+
+def test_divisao():
+    calculadora = Calculadora()
+    resultado = calculadora.divisao(6, 3)
+    assert resultado == 2
+
+def test_divisao_por_zero():
+    calculadora = Calculadora()
+    resultado = calculadora.divisao(6, 0)
+    assert resultado == "Erro: divisão por zero"
